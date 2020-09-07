202c8a4ec255ecc335268e2bb0c9b587c3aa6b68
sksrivstava <sksrivstava@rediffmail.com>
2020-09-07 19:09:15 +0530

    updated

---
+++ FileService/Controllers/FileController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileService.Controllers
+{
+    public class FileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //public ActionResult Import(ImportRequest importRequest)
+        //{
+        //    importRequest.Url = "http://mvc.syncfusion.com/Spreadsheet/LargeData.xlsx";
+        //    return importRequest.SpreadsheetActions();
+        //}
+    }
+
+    public class ImportRequest
+    {
+    }
+}
