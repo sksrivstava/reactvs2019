881c3202fc3106332432d56778ff25b6a1e590f3
sksrivstava <sksrivstava@rediffmail.com>
2020-09-11 20:57:09 +0530

    revised source

--- dotnetReact/ClientApp/src/components/FetchSpreadData.js
+++ dotnetReact/ClientApp/src/components/FetchSpreadData.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import { ApplicationState } from '../store';
 import * as WeatherForecastsStore from '../store/WeatherForecasts';
 import $ from 'jquery';
-
+import { DataManager, Query } from '@syncfusion/ej2-data';
 import {
     SpreadsheetComponent,
     SheetsDirective,
@@ -22,18 +22,18 @@ import {
 //import { data } from "../Data/DataSource";
 import "./sreadsheet.css";
 
-    // Save excel file to the server.
-    function saveAsExcel(args) {
-        var xlObj = $("#Spreadsheet").data("ejSpreadsheet"), fileName = $("#fileName").val(), exportProp = xlObj.XLExport.getExportProps();
-        $.ajax({
-        type: "POST",
-            url: "/Spreadsheet/saveAsExcel",
-            data: {fileName: fileName, sheetModel: exportProp.model, sheetData: exportProp.data },
-            success: function () {
-        // Success code here.
-    }
-        });
-    }
+    //// Save excel file to the server.
+    //function saveAsExcel(args) {
+    //    var xlObj = $("#Spreadsheet").data("ejSpreadsheet"), fileName = $("#fileName").val(), exportProp = xlObj.XLExport.getExportProps();
+    //    $.ajax({
+    //    type: "POST",
+    //        url: "/Spreadsheet/saveAsExcel",
+    //        data: {fileName: fileName, sheetModel: exportProp.model, sheetData: exportProp.data },
+    //        success: function () {
+    //    // Success code here.
+    //}
+    //    });
+    //}
 
     // Load excel file from the server to the Spreadsheet.
 const state = {
@@ -43,57 +43,101 @@ const state = {
  
 
 export class FetchSpreadData extends SpreadsheetComponent {
-    constructor() {
-        super(...arguments);
+    //constructor() {
+    //    super(...arguments);
+    //    this.query = new Query().
+    //        select(['OrderID', 'CustomerID', 'ShipName', 'ShipCity', 'ShipCountry', 'Freight']).take(100);
+    //    this.data = new DataManager({
+    //        url: 'https://js.syncfusion.com/demos/ejServices//wcf/Northwind.svc/Orders',
+    //        crossDomain: true
+    //    });
+    //    this.boldRight = { fontWeight: "bold", textAlign: "right" };
+    //    this.bold = { fontWeight: "bold" };
+
+    //}
+    constructor(props) {
+        super(props);
         this.boldRight = { fontWeight: "bold", textAlign: "right" };
         this.bold = { fontWeight: "bold" };
+        this.state = {
+            dataexl: {}
+        }
+       
+    }
+    
+    saveFile() {
+       var resp = this.spreadsheet.save({ fileName: "Sample" });
+    }
+    loadFile() {
+        let request = new XMLHttpRequest();
+        request.responseType = "blob";
+        request.onload = () => {
+            let file = new File([request.response], 'Sample' + ".xlsx");
+            this.spreadsheet.open({ file: file });
+        }
+        request.open("GET", "http://localhost:53142/Files/" + 'Sample1' + ".xlsx");
+        request.send();
     }
     componentDidMount() {
-        var filenm = "Samples1.xlsx";
-        var xlObj = $("#Spreadsheet").data("ejSpreadsheet"), fileName = filenm;
-       
-        $.ajax({
-            type: "POST",
-            url: "http://localhost:57038/api/Fileopern",
-            data: { fileName: filenm },
-            success: function (data) {
-                //console.log(data);
-                //this.setState({
-                //data: data
-           // });
-               // xlObj.loadFromJSON(data);
-            }
-        });
+        let request = new XMLHttpRequest();
+        request.responseType = "blob";
+        request.onload = () => {
+            let file = new File([request.response], 'Sample' + ".xlsx");
+            this.spreadsheet.open({ file: file });
+        }
+        request.open("GET", "http://localhost:53142/Files/" + 'Sample' + ".xlsx");
+        request.send();
+        this.spreadsheet.isEdit = false;
     }
 
-    loadExcel(args) {
-        var filenm = "Samples1.xlsx";
-       // var xlObj = $("#Spreadsheet").data("ejSpreadsheet"), fileName = $("#fileName").val();
-        var xlObj = $("#Spreadsheet").data("ejSpreadsheet"), fileName = filenm;
-        //$.ajax({
-        //    url: "http://localhost:57038/api/Fileopern",
-        //    data: { fileName: fileName },
-        //    async: true,
-        //    dataType: 'jsonp'   //you may use jsonp for cross origin request
+    //componentDidMount() {
+    //    var filenm = "Samples1.xlsx";
+    //   // var xlObj = $("#Spreadsheet").data("ejSpreadsheet"), fileName = filenm;
+    //    var xlObj = document.getElementById("spreadsheet"), fileName = filenm;
+   
+    //    //$.ajax({
+    //    //    type: "POST",
+    //    //    url: "http://localhost:57038/api/Fileopern",
+    //    //    data: { fileName: filenm },
+    //    //    success: function (data) {
+    //    //        console.log(data);
+    //    //        this.setState({
+    //    //            dataexl: data
+    //    //        })
+    //    //      //  xlObj.loadFromJSON(data);
+    //    //    }
+    //    //});
+    //}
+
+    //loadExcel(args) {
+    //    var filenm = "Samples1.xlsx";
+    //   // var xlObj = $("#Spreadsheet").data("ejSpreadsheet"), fileName = $("#fileName").val();
+    //    var xlObj = $("#spreadsheet").data("ejSpreadsheet"), fileName = filenm;
+    //    //$.ajax({
+    //    //    url: "http://localhost:57038/api/Fileopern",
+    //    //    data: { fileName: fileName },
+    //    //    async: true,
+    //    //    dataType: 'jsonp'   //you may use jsonp for cross origin request
            
-        //}).then(function (data) {
-        //    //this.setState({
-        //    //    data: data
-        //    //});
-        //    xlObj.loadFromJSON(data);
+    //    //}).then(function (data) {
+    //    //    //this.setState({
+    //    //    //    data: data
+    //    //    //});
+    //    //    xlObj.loadFromJSON(data);
 
-        //});
+    //    //});
 
-        $.ajax({
-            type: "POST",
-             url: "http://localhost:57038/api/Fileopern",
-           data: { fileName: fileName },
-            success: function (data) {
-                console.log(data);
-                xlObj.loadFromJSON(data);
-            }
-        });
-    }
+    //    $.ajax({
+    //        type: "POST",
+    //         url: "http://localhost:57038/api/Fileopern",
+    //       data: { fileName: fileName },
+    //        success: function (data) {
+    //            console.log(data);
+    //            xlObj.loadFromJSON(JSON.parse(data));
+    //           // xlObj.loadFromJSON(data);
+    //        }
+    //    });
+    //}
 
 //    <script>
 //    $(function () {
@@ -114,7 +158,8 @@ export class FetchSpreadData extends SpreadsheetComponent {
 
         );
         this.spreadsheet.numberFormat("$#,##0.00", "F2:F31");
-        this.spreadsheet.open("http://localhost:55008/Samples1.xlsx");
+        this.spreadsheet.allowEditing = false; 
+        //this.spreadsheet.open("http://localhost:55008/Samples1.xlsx");
         //var aa=   this.spreadsheet.ejSpreadsheet({
         //    allowImport: true,
         //    importSettings: {    sheetData={this.state.data}
@@ -129,14 +174,30 @@ export class FetchSpreadData extends SpreadsheetComponent {
         return (
             <div>
                 <div>
-                    
-                    <button onClick={this.loadExcel}>Load file</button></div>
+                     <button class='e-btn' onClick={this.saveFile.bind(this)}>Save as Excel</button>
+                    <button class='e-btn' onClick={this.loadFile.bind(this)}>Load Excel</button>
+                    </div>
+                <div id="spreadsheet"></div>
             <div className="control-pane">
                 <div className="control-section spreadsheet-control">
-                    <SpreadsheetComponent>
-                        <spreadsheet id="Spreadsheet" allowImport="true">
-                            
-                        </spreadsheet>
+                        <SpreadsheetComponent openUrl='http://localhost:53142/Home/Open'
+                            saveUrl='http://localhost:53142/Home/Save' ref={(ssObj) => { this.spreadsheet = ssObj; }}
+                            cellEdit={false}
+                            editSettings={true}>
+                            <SheetsDirective>
+                                <SheetDirective name='Shipment Details'>
+                                    <RangesDirective>
+                                        <RangeDirective dataSource={this.data} query={this.query}></RangeDirective>
+                                    </RangesDirective>
+                                    <ColumnsDirective>
+                                        <ColumnDirective width={100}></ColumnDirective>
+                                        <ColumnDirective width={130}></ColumnDirective>
+                                        <ColumnDirective width={150}></ColumnDirective>
+                                        <ColumnDirective width={200}></ColumnDirective>
+                                        <ColumnDirective width={180}></ColumnDirective>
+                                    </ColumnsDirective>
+                                </SheetDirective>
+                            </SheetsDirective>
                     </SpreadsheetComponent>
 
 
